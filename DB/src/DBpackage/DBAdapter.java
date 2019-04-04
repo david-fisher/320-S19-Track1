31e47d8c2fb2efdf22df28e6cb22de2c8a057fe4
KyuMin Kim <caroline@guest172.publicnet.localdomain>
2019-04-04 15:00:14 -0400

    Structure for DBAdapter added

--- DB/src/DBpackage/DBAdapter.java
+++ DB/src/DBpackage/DBAdapter.java
@@ -4,24 +4,58 @@ import java.sql.*;
 
 
 public class DBAdapter {
-	public boolean createUser(JSONObject usr){ 
+	public JSONObject createUser(JSONObject usr){
 		//stub
-		return true;
+		return null;
 	}
-	
+
 	public JSONObject readUser(String id) {
 		//stub
 		return new JSONObject();
 	}
-	
+
 	public JSONObject updateUser(String id, JSONObject data) {
 		//stub
 		return new JSONObject();
 	}
-	
+
 	public boolean deleteUser(String id) {
 		//stub
 		return true;
 	}
-	
+
+	public JSONObject[] getPopularPosts() {
+		//stub
+		return null;
+	}
+
+	public JSONObject[] getFlaggedPosts() {
+		//stub
+		return null;
+	}
+
+	public JSONObject[] hashtagSearch(String str) {
+		//stub
+		return null;
+	}
+
+	public JSONObject createPost(JSONObject post) {
+		//stub
+		return new JSONObject();
+	}
+
+	public JSONObject readPost(String str) {
+		//stub
+		return new JSONObject();
+	}
+
+	public JSONObject updatePost(String str, JSONObject post) {
+		//stub
+		return new JSONObject();
+	}
+
+	public boolean deletePost(JSONObject post){
+		//stub
+		return true;
+	}
 }
