abefad045b362c036b1d85ac435c44de942be336
Alexander Vukovic <alexvukovic96@gmail.com>
2019-04-06 15:25:55 -0400

    lol jk I forgot to fix a typo

--- DB/src/DBpackage/DBAdapter.java
+++ DB/src/DBpackage/DBAdapter.java
@@ -6,7 +6,7 @@ import java.sql.*;
 
 public class DBAdapter {
 	
-	private String DBAddress = null;
+	private String DBAddress = null; //access address for database. figure out l8tr
 	
 	private Connection getConnection() {
 		//function used to get connection to database
