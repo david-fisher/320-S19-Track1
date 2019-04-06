3bf3275331955d8a16df16f5d7072191a9e19a97
Alexander Vukovic <alexvukovic96@gmail.com>
2019-04-06 14:16:37 -0400

    added all user functions

--- DB/src/DBpackage/DBAdapter.java
+++ DB/src/DBpackage/DBAdapter.java
@@ -4,21 +4,156 @@ import java.sql.*;
 
 
 public class DBAdapter {
+	
+	private String DBAddress = null;
+	
+	private Connection getConnection() {
+		//function used to get connection to database
+		//stub
+		return null;
+	}
+	
+	/*
+	 * USER FUNCTIONS - GETTERS & SETTERS FOR ALL FIELDS
+	 * 
+	 */
 	public JSONObject createUser(JSONObject usr){
 		//stub
 		return null;
 	}
 
-	public JSONObject readUser(String id) {
+	public JSONObject getUser(String id) { 
+		//general function- returns entire user.
 		//stub
 		return null;
 	}
-
-	public JSONObject updateUser(String id, JSONObject data) {
+	
+	public boolean setUser(String id, JSONObject user) { 
+		//general function - sets entire user based on new json object.
+		//stub
+		return true;
+	}
+	
+	public String getUserType(String id) {
 		//stub
 		return null;
 	}
+	
+	public boolean setUserType(String id, String type) {
+		//stub
+		return true;
+	}
 
+	public String getUserName(String id) {
+		//stub
+		return null;
+	}
+	
+	public boolean setUserName(String id, String name) {
+		//stub
+		return true;
+	}
+	
+	public JSONObject getUserAddress(String id) {
+		//stub
+		return null;
+	}
+	
+	public boolean setUserAddress(String id, JSONObject address) {
+		//stub
+		return true;
+	}
+	
+	public int getUserPhone(String id) {
+		//stub
+		return 0;
+	}
+	
+	public boolean setUserPhone(String id, int number) {
+		//stub
+		return true;
+	}
+	
+	public String getUserBirthday(String id) {
+		//stub
+		return null;
+	}
+	
+	public boolean setUserBirthday(String id, String bday) {
+		//stub
+		return true;
+	}
+	
+	public int getUserPoints(String id) {
+		//stub
+		return 0;
+	}
+	
+	public boolean setUserPoints(String id, int amt) {
+		//stub
+		return true;
+	}
+	
+	public int[] getUserFollowing(String id) {
+		//stub
+		return null;
+	}
+	
+	public boolean setUserFollowing(String id, int[] users) {
+		//stub
+		return true;
+	}
+	
+	public int getUserInviter(String id) {
+		//stub
+		return 0;
+	}
+	
+	public boolean setUserInviter(String id, int inviter) {
+		//stub
+		return true;
+	}
+	
+	public int[] getUsersInvited(String id) {
+		//stub
+		return null;
+	}
+	
+	public boolean setUsersInvited(String id, int[] users) {
+		//stub
+		return true;
+	}
+	
+	public boolean getUserHasInvited(String id) {
+		//stub
+		return true;
+	}
+	
+	public boolean setUserHasInvited(String id, boolean status) {
+		//stub
+		return true;
+	}
+	
+	public JSONObject getUserCard(String id) {
+		//stub
+		return null;
+	}
+	
+	public boolean setUserCard(String id, JSONObject card) {
+		//stub
+		return true;
+	}
+	
+	public boolean getUserPrivate(String id){
+		//stub
+		return true;
+	}
+
+	public boolean setUserPrivate(String id, boolean state) {
+		//stub
+		return true;
+	}
+	
 	public boolean deleteUser(String id) {
 		//stub
 		return true;
@@ -30,11 +165,6 @@ public class DBAdapter {
 	}
 
 	public JSONObject[] getFlaggedPosts() {
-		//stub
-		return null;
-	}
-
-	public JSONObject[] hashtagSearch(String str) {
 		//stub
 		return null;
 	}
@@ -57,5 +187,15 @@ public class DBAdapter {
 	public boolean deletePost(JSONObject post){
 		//stub
 		return true;
+	}
+	
+	public boolean createURL (JSONObject url) {
+		//stub
+		return true;
+	}
+	
+	public String getOrgURL (String id) {
+		//stub
+		return null;
 	}
 }
