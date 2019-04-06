3a81f01eaa93b0567d3a7f2ce906706a3db51da2
Alexander Vukovic <alexvukovic96@gmail.com>
2019-04-06 15:41:48 -0400

    made an actual test suite this time

--- DB/src/DBpackage/DBTestSuite.java
+++ DB/src/DBpackage/DBTestSuite.java
@@ -1,6 +1,16 @@
 package DBpackage;
-//import org.j
 
-public class DBTestSuite {
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DBTestSuite {
+	DBAdapter database = new DBAdapter();
+	
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
 
 }
