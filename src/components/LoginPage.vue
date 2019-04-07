e209bc9c099408da3d91fb0309b500c0f3593341
mfclinton <mfclinton@umass.edu>
2019-04-07 19:51:38 -0400

    made login page

---
+++ src/components/LoginPage.vue
@@ -0,0 +1,31 @@
+<template>
+<h1>{{ msg }}</h1>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'This is a Test'
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import LoginPage from '@/components/LoginPage'
 
 Vue.use(Router)
 
