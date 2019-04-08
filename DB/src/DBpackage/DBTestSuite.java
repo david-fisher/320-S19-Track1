c33cb1c0cc282e5c6c4dd68229afc61421f25246
Alexander Vukovic <alexvukovic96@gmail.com>
2019-04-08 10:10:26 -0400

    added user test stubs

--- DB/src/DBpackage/DBTestSuite.java
+++ DB/src/DBpackage/DBTestSuite.java
@@ -5,12 +5,457 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class DBTestSuite {
-	DBAdapter database = new DBAdapter();
-	
+	//declare database instance and data here
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void connectionTest(){
+		//stub
 	}
-
+	
+/*
+* USER TESTS
+*/
+	
+	@Test
+	public void createUserIncompleteData(){
+		//stub
+	}
+	
+	@Test
+	public void createUserCompleteData(){
+		//stub
+	}
+	
+	@Test
+	public void getNonexistingUser(){
+		//stub
+	}
+	
+	@Test
+	public void getExistingUser(){
+		//stub
+	}
+	
+	@Test
+	public void setExistingUserValidInfo(){
+		//stub
+	}
+	
+	@Test
+	public void setExistingUserInvalidInfo(){
+		//stub
+	}
+	
+	@Test
+	public void setUnexistingUserValidInfo(){
+		//stub
+	}
+	
+	
+	@Test
+	public void setUnexistingUserInvalidInfo(){
+		//stub
+	}
+	
+	@Test
+	public void getExistingUserType(){
+		//stub
+	}
+	
+	@Test
+	public void getUnexistingUserType(){
+		//stub
+	}
+	
+	@Test
+	public void setExistingUserValidType(){
+		//stub
+	}
+	
+	@Test
+	public void setExistingUserInvalidType(){
+		//stub
+	}
+	
+	@Test
+	public void setUnexistingUserValidType(){
+		//stub
+	}
+	
+	@Test
+	public void setUnexistingUserInvalidType(){
+		//stub
+	}
+	
+	@Test
+	public void getExistingUserName(){
+		//stub
+	}
+	
+	@Test
+	public void getUnexistingUserName(){
+		//stub
+	}
+	
+	@Test
+	public void setExistingUserValidName(){
+		//stub
+	}
+	
+	@Test
+	public void setExistingUserInvalidName(){
+		//stub
+	}
+	
+	@Test
+	public void setUnexistingUserValidName(){
+		//stub
+	}
+	
+	@Test
+	public void setUnexistingUserInvalidName(){
+		//stub
+	}
+	
+	@Test
+	public void getExistingUserAddress(){
+		//stub
+	}
+	
+	@Test
+	public void getUnexistingUserAddress(){
+		//stub
+	}
+	
+	@Test
+	public void setExistingUserValidAddress(){
+		//stub
+	}
+	
+	@Test
+	public void setExistingUserInvalidAddress(){
+		//stub
+	}
+	
+	@Test
+	public void setUnexistingUserValidAddress(){
+		//stub
+	}
+	
+	@Test
+	public void setUnexistingUserInvalidAddress(){
+		//stub
+	}
+	
+	@Test
+	public void getExistingUserPhone(){
+		//stub
+	}
+	
+	@Test
+	public void getUnexistingUserPhone(){
+		//stub
+	}
+	
+	@Test
+	public void setExistingUserValidPhone(){
+		//stub
+	}
+	
+	@Test
+	public void setExistingUserInvalidPhone(){
+		//stub
+	}
+	
+	@Test
+	public void setUnexistingUserValidPhone(){
+		//stub
+	}
+	
+	@Test
+	public void setUnexistingUserInvalidPhone(){
+		//stub
+	}
+	
+	@Test
+	public void getExistingUserBirthday(){
+		//stub
+	}
+	
+	@Test
+	public void getUnexistingUserBirthday(){
+		//stub
+	}
+	
+	@Test
+	public void setExistingUserValidBirthday(){
+		//stub
+	}
+	
+	@Test
+	public void setExistingUserInvalidBirthday(){
+		//stub
+	}
+	
+	@Test
+	public void setUnexistingUserValidBirthday(){
+		//stub
+	}
+	
+	@Test
+	public void setUnexistingUserInvalidBirthday(){
+		//stub
+	}
+	
+	@Test
+	public void getExistingUserPoints(){
+		//stub
+	}
+	
+	@Test
+	public void getUnexistingUserPoints(){
+		//stub
+	}
+	
+	@Test
+	public void setExistingUserValidPoints(){
+		//stub
+	}
+	
+	@Test
+	public void setExistingUserInvalidPoints(){
+		//stub
+	}
+	
+	@Test
+	public void setUnexistingUserValidPoints(){
+		//stub
+	}
+	
+	@Test
+	public void setUnexistingUserInvalidPoints(){
+		//stub
+	}
+	
+	@Test
+	public void getExistingUserFollowing(){
+		//stub
+	}
+	
+	@Test
+	public void getUnexistingUserFollowing(){
+		//stub
+	}
+	
+	@Test
+	public void setExistingUserValidFollowing(){
+		//stub
+	}
+	
+	@Test
+	public void setExistingUserInvalidFollowing(){
+		//stub
+	}
+	
+	@Test
+	public void setUnexistingUserValidFollowing(){
+		//stub
+	}
+	
+	@Test
+	public void setUnexistingUserInvalidFollowing(){
+		//stub
+	}
+	
+	@Test
+	public void getExistingUserInviter(){
+		//stub
+	}
+	
+	@Test
+	public void getUnexistingUserInviter(){
+		//stub
+	}
+	
+	@Test
+	public void setExistingUserValidInviter(){
+		//stub
+	}
+	
+	@Test
+	public void setExistingUserInvalidInviter(){
+		//stub
+	}
+	
+	@Test
+	public void setUnexistingUserValidInviter(){
+		//stub
+	}
+	
+	@Test
+	public void setUnexistingUserInvalidInviter(){
+		//stub
+	}
+	
+	@Test
+	public void getExistingUserInvitedUsers(){
+		//stub
+	}
+	
+	@Test
+	public void getUnexistingUserInvitedUsers(){
+		//stub
+	}
+	
+	@Test
+	public void setExistingUserValidInvitedUsers(){
+		//stub
+	}
+	
+	@Test
+	public void setExistingUserInvalidInvitedUsers(){
+		//stub
+	}
+	
+	@Test
+	public void setUnexistingUserValidInvitedUsers(){
+		//stub
+	}
+	
+	@Test
+	public void setUnexistingUserInvalidInvitedUsers(){
+		//stub
+	}
+	
+	@Test
+	public void getExistingUserCardInfo(){
+		//stub
+	}
+	
+	@Test
+	public void getUnexistingUserCardInfo(){
+		//stub
+	}
+	
+	@Test
+	public void setExistingUserValidCardInfo(){
+		//stub
+	}
+	
+	@Test
+	public void setExistingUserInvalidCardInfo(){
+		//stub
+	}
+	
+	@Test
+	public void setUnexistingUserValidCardInfo(){
+		//stub
+	}
+	
+	@Test
+	public void setUnexistingUserInvalidCardInfo(){
+		//stub
+	}
+	
+	@Test
+	public void getExistingUserCanLogin(){
+		//stub
+	}
+	
+	@Test
+	public void getUnexistingUserCanLogin(){
+		//stub
+	}
+	
+	@Test
+	public void setExistingUserValidCanLogin(){
+		//stub
+	}
+	
+	@Test
+	public void setExistingUserInvalidCanLogin(){
+		//stub
+	}
+	
+	@Test
+	public void setUnexistingUserValidCanLogin(){
+		//stub
+	}
+	
+	@Test
+	public void setUnexistingUserInvalidCanLogin(){
+		//stub
+	}
+	
+	@Test
+	public void getExistingUserPrivacy(){
+		//stub
+	}
+	
+	@Test
+	public void getUnexistingUserPrivacy(){
+		//stub
+	}
+	
+	@Test
+	public void setExistingUserValidPrivacy(){
+		//stub
+	}
+	
+	@Test
+	public void setExistingUserInvalidPrivacy(){
+		//stub
+	}
+	
+	@Test
+	public void setUnexistingUserValidPrivacy(){
+		//stub
+	}
+	
+	@Test
+	public void setUnexistingUserInvalidPrivacy(){
+		//stub
+	}
+	
+	@Test
+	public void getExistingUserPosts(){
+		//stub
+	}
+	
+	@Test
+	public void getUnexistingUserPosts(){
+		//stub
+	}
+	
+	@Test
+	public void setExistingUserValidPosts(){
+		//stub
+	}
+	
+	@Test
+	public void setExistingUserInvalidPosts(){
+		//stub
+	}
+	
+	@Test
+	public void setUnexistingUserValidPosts(){
+		//stub
+	}
+	
+	@Test
+	public void setUnexistingUserInvalidPosts(){
+		//stub
+	}
+	
+	
+	@Test
+	public void deleteExistingUser(){
+		//stub
+	}
+	
+	@Test
+	public void deleteUnexistingUser(){
+		//stub
+	}
+	
 }
