182b69e17e608e862f5927542194184e919c2593
ovidal605 <ovidal605@gmail.com>
2019-04-18 12:41:36 -0400

    Added constructor for post

--- DB/src/DBpackage/Post.java
+++ DB/src/DBpackage/Post.java
@@ -1,9 +1,35 @@
 package DBpackage;
+import java.io.File;
+import java.util.Date;
 
 public class Post {
 	//Instance variables
 	private User user;
 	private int postId;
+	private String type;
+	private Date date;
+	private String caption;
+	private int[] comments;
+	private boolean explicit;
+	private boolean visible;
+	private int tags;
+	private File originalPhoto;
+	private int parent;
 	
+	//Constructor
+	public Post(User user, int postId, String type, Date date, String caption, int[] comments,
+			boolean explicit, boolean visible, int tags, File originalPhoto, int parent){
+		this.user = user;
+		this.postId = postId;
+		this.type = type;
+		this.date = date;
+		this.caption = caption;
+		this.comments = comments;
+		this.explicit = explicit;
+		this.visible = visible;
+		this.tags = tags;
+		this.originalPhoto = originalPhoto;
+		this.parent = parent;
+	}
 	
 }
