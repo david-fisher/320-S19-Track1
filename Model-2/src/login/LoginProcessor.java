6e12c0be0ec334df39d131fea43964e3be18a2b8
DavidKoleczek <davidkoleczek@gmail.com>
2019-04-28 14:08:59 -0400

    Restructuring Tests
    Added login + registration test folders

--- Model-2/src/login/LoginProcessor.java
+++ Model-2/src/login/LoginProcessor.java
@@ -69,30 +69,4 @@ public class LoginProcessor {
 		}
 	}
 
-	/* LoginProcessor - Testing */
-	
-	@Test
-	public void testLoginCorrect() {
-	   // Checks with WS & DB API to see correct credentials 
-	   // work to log a User in
-	}
-
-	@Test
-	public void testLoginIncorrect() {
-	   // Checks with WS & DB API to see if wrong credentials 
-	   // still allow a User to be logged in
-	}
-
-	@Test
-	public void testCookieMonster() {
-	   // Checks with web server if a new session 
-	   // is created after calling createSessionCookie()
-	}
-
-	@Test
-	public void testPassReset() {
-	   // Checks with DB API to see if password is reset 
-	   // after calling resetPassword()
-	}
-
 }
