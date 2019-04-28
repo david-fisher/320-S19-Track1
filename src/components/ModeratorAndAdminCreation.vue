66f5cc7cff892500a619a079bd12c50c65906f92
mfclinton <mfclinton@umass.edu>
2019-04-28 14:39:58 -0400

    finished mod and admin adding

--- src/components/ModeratorAndAdminCreation.vue
+++ src/components/ModeratorAndAdminCreation.vue
@@ -10,6 +10,16 @@
         </select>
       </div>
       <br>
+      <div class="ui fluid input">
+        <label class="label" for="username"><b>Username: </b></label>
+        <input type="text" v-model="form.name" placeholder="Username" id="username" required>
+      </div>
+      <br>
+      <div class="ui fluid input">
+        <label class="label" for="password"><b>Password: </b></label>
+        <input type="password" v-model="form.pass" placeholder="Password" id="password" required>
+      </div>
+      <br>
       <div>
         <button type="submit" style="width:150px; margin-bottom:5px; color:#D6A200" class="ui black button">Create</button>
       </div>
