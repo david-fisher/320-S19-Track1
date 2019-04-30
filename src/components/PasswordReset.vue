0cad5b4a0d674cc69911e2abac4d67e5b4dbfd24
Th3OnlyN00b <pathos.smith@gmail.com>
2019-04-30 14:56:25 -0400

    removed emailing of codes for users

--- src/components/PasswordReset.vue
+++ src/components/PasswordReset.vue
@@ -1,33 +1,18 @@
 <template>
   <div align="center" style="border:1px solid black">
     <h1>Reset Password</h1>
-    <form class="form" v-on:submit="sendCode($event)">
-      <div>
-        <div style="text-align: left">
-          <label  class="label" align="left" style="margin-inline-start: 20%;" for="email"><b>Email: </b></label>
-        </div>
-        <div class="ui fluid input">
-          <input type="email" v-model="form.email" placeholder="Email" id="email" required>
-        </div>
-      </div>
-      <br>
-      <div>
-        <button type="submit" style="width:200px; margin-bottom:5px; color:#D6A200" class="ui black button">Send Verification Code</button>
-      </div>
-    </form>
-    <br>
     <form class="form" v-on:submit="sendPass($event)">
       <div style="text-align: left">
         <label  class="label" align="left" style="margin-inline-start: 20%;"><b>Info: </b></label>
       </div>
       <div class="ui fluid input">
-        <input v-model="form2.code" placeholder="Verification Code" id="vcode" required>
+        <input v-model="form2.email" placeholder="Email" id="email" required>
         <input type="password" v-model="form2.pass" placeholder="New password" id="pass" required>
         <input type="password" v-model="form2.confPass" placeholder="Confirm password" id="cpass" required>
       </div>
       <br>
       <div>
-        <button type="submit" :disabled="disabled" style="width:200px; margin-bottom:5px; color:#D6A200" class="ui black button">Reset Password</button>
+        <button type="submit" style="width:200px; margin-bottom:5px; color:#D6A200" class="ui black button">Reset Password</button>
       </div>
       <div class="submission check">
         <p style="color:#FF0000">{{ submitText }}</p>
@@ -41,15 +26,11 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      form: {
-        email: ''
-      },
       form2: {
-        code: '',
+        email:'',
         pass: '',
         confPass: ''
       },
-      disabled: true,
       submitText: ''
     }
   },
@@ -58,35 +39,9 @@ export default {
       return /\S+@\S+\.\S+/.test(this.form.email)
     },
 
-
-    sendResetCode: function(){
-      const path = this.ip + '/sendResetCode'
-
-      this.$http.post(path, this.form)
-      .then(response => {
-        var retVal = JSON.parse('{'+response.bodyText)
-        console.log(retVal)
-
-        //All is well
-        if(retVal.result.length == 0){
-          this.submitText = "Code Sent!"
-          this.disabled = false
-        } else {
-          this.submitText = "somethin"
-        }
-      })
-      .catch(error => {
-        console.log("Yeah nope")
-        this.submitText = "Error processing request. Please try again."
-        console.log(error)
-      })
-
-    },
-
-
     submit: function(){
       if(!this.checkEmail()){
-        this.form.submitText = 'Invalid email'
+        this.submitText = 'Invalid email'
       } else {
         this.sendResetCode()
       }
