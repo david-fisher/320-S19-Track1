db9d8122ea828d2010319802e58aab8f1ab68569
MatthewGimlewicz <gimlewiczm@gmail.com>
2019-05-02 17:38:04 -0400

    included url tests

--- Model-2/test/post/URL_Test.java
+++ Model-2/test/post/URL_Test.java
@@ -1,5 +1,27 @@
 package post;
 
-public class URL_Test {
+import org.junit.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class URL_Test {
+    @Test
+    public void testURLCreation() {
+        String original = "www.helloworld.com";
+        String shortened = URLShortner.shorten(original);
+        assertEquals(shortened.length(), 23);
+    }
+
+    @Test
+    public void testShortenCode() {
+        String shortened = URLShortner.shorten("~");
+        assertEquals(shortened.length()-18,5);
+    }
+
+    @Test
+    public void testShortenFormat() {
+        String original = "www.helloworld.com";
+        String shortened = URLShortner.shorten(original);
+        assertEquals(shortened.substring(0,18),"membersonly.com/l/");
+    }
 }
