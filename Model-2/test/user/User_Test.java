c49b4605364a1fac973306454734bbe6ffa2c5ee
Jacob Calkins <jcalkins@umass.edu>
2019-05-02 22:06:01 -0400

    the testening part 2

--- Model-2/test/user/User_Test.java
+++ Model-2/test/user/User_Test.java
@@ -1,6 +1,7 @@
 package user;
 
 import db.DBAdapter;
+import db.Database;
 import user.*;
 import stripe.*;
 import org.junit.jupiter.api.Test;
@@ -9,65 +10,27 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 public class User_Test {
 
-    String email = "test@gmail.com";
-    String cardNum = "4000056655665556";
-    String zipCode = "00000";
-    String cvv = "123";
-    String exp_month = "10";
-    String exp_year = "2020";
-
-    // Create the test card with the information above
-    StripeCreditCard testCard = new StripeCreditCard(email, cardNum, zipCode, cvv, exp_month, exp_year);
+    // User to test on
+    User testUser = new User("test@umass.edu", "test", "umass", 1000, null, "member", null);
+    public DBAdapter database = Database.adapter;
     /**
-     * Calls CC adapter to charge User’s card,
-     * verifies if the card is charged
+     *  Tests whether information can be updated for any field
+     *  in the DB.
+     *  Evaluates to correct if information is updated correctly.
      */
+
     @Test
-    public void testCCCharging() {
-
-        //Create new user to test on
-        User testerCC = new User("testEmail", "firstName", "lastName", 1000, null, "member",testCard);
-
-        //Call CC adapter to charge user card
-        boolean result = false;
-        //if(testerCC.charge().notEquals("GENERIC STRIPE ERROR")) result = true;
-        //assertTrue(result);
-    }
-
-    /**
-     * Calls DB API to determine if User point
-     * total reflects a specific number of points added
-     */
-    @Test
-    public void testAddPoints() {
-        //Create new user to test on
-        User testerPoints = new User("testEmail2", "firstName2", "lastName2", 1000, null, "member",testCard);
-
-        // Call the User addPoints() method
-        //testerCC2.addPoints(10);
-
-        // Check that the points added to the User is correct
-        //assertEquals(1010, DBAdapter.getPoints(testerCC2.email));
-
-    }
-
-    /**
-     * Calls DB API to determine if User credentials
-     * reflect a specific change as made by updateAccountInfo()
-     * As of right now settings only contains: - creditCard info
-     * 										   - postVisibility
-     * 										   - profileDescription
-     */
-    /*@Test
     public void testInfoUpdated() {
-    	//Create new user to test on
-    	User testerInfo = new User("testEmail3", "firstName3", "lastName3", 1000, null, "member");
+    	String fieldString = "";
+    	Object data = null;
 
-    	//Have the DB set new credit card
-    	JSONObject card = new JSONObject(); // stub
-        DBAdapter.setUserCard(testerInfo,card); //unfortunately does not exist/work yet
+    	// Update the field
+        database.updateUser(this.testUser.email, fieldString, data);
+
+        // Grab a copy of the user
+        User newTestedUser = database.getUser(this.testUser.email);
 
         //Check all credentials are correct
-        assertEquals(card, DBAdapter.getUserCard(testerInfo));
-    }*/
+        //assertEquals(newTestedUser.fieldString, data);
+    }
 }
